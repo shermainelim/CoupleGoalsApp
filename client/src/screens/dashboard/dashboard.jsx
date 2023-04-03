70b0a6358e941023824708b814701cdc6b097abd
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-04-03 22:44:06 +0800

    chore: refactor functions shift to utils.js
    - make functions reusable

--- client/src/screens/dashboard/dashboard.jsx
+++ client/src/screens/dashboard/dashboard.jsx
@@ -25,7 +25,7 @@ import {
 import { Navigate } from "react-router-dom";
 import moment from "moment";
 import { useNavigate } from "react-router-dom";
-import randomIntFromInterval from "../../utils/cgUtil";
+import * as cgUtils from "../../utils/cgUtil"
 import Couple from "../../assets/couple3.png"
 
 const Dashboard = () => {
@@ -86,7 +86,6 @@ useEffect(()=>{
     let fetchFinanceDataProcessed = fetchFinanceData[0];
     setToDoFinance(fetchFinanceDataProcessed);
 
-   
   }
   
   
@@ -122,7 +121,6 @@ useEffect(()=>{
     }
   }
 
- 
 
   function sortedArr() {
     if (newArr.length === 0) {
@@ -137,8 +135,6 @@ useEffect(()=>{
       });
     });
   }
-
-
 
   //first person login
   
@@ -162,39 +158,12 @@ useEffect(()=>{
   }
 
  
-  function getNumberOfDays(start) {
-    const date1 = new Date(start);
-    const date2 = new Date();
-
-    // One day in milliseconds
-    const oneDay = 1000 * 60 * 60 * 24;
-
-    // Calculating the time difference between two dates
-    const diffInTime = date2.getTime() - date1.getTime();
-
-    // Calculating the no. of days between two dates
-    const diffInDays = Math.round(diffInTime / oneDay);
-
-    return diffInDays;
-  }
 
   // mm.dd.yyyy
-  let daysTgt = getNumberOfDays(anniversaryDateFirstPersonUser);
+  let daysTgt = cgUtils.getNumberOfDays(anniversaryDateFirstPersonUser);
 
-  function getFormatedStringFromDays(numberOfDays) {
-    var years = Math.floor(numberOfDays / 365);
-    var months = Math.floor((numberOfDays % 365) / 30);
-    var days = Math.floor((numberOfDays % 365) % 30);
 
-    var yearsDisplay =
-      years > 0 ? years + (years == 1 ? " Year, " : " Years, ") : "";
-    var monthsDisplay =
-      months > 0 ? months + (months == 1 ? " Month, " : " Months, ") : "";
-    var daysDisplay = days > 0 ? days + (days == 1 ? " Day" : " Days") : "";
-    return yearsDisplay + monthsDisplay + daysDisplay;
-  }
-
-  const yearsTgt = getFormatedStringFromDays(daysTgt);
+  const yearsTgt = cgUtils.getFormatedStringFromDays(daysTgt);
 
 
 
@@ -202,7 +171,7 @@ useEffect(()=>{
   ///////////
   const addTask = () => {
     if (newTask) {
-      let num = randomIntFromInterval(1, 10000000);
+      let num = cgUtils.randomIntFromInt(1, 10000000);
       setToDo([...toDo, { id: num, title: newTask, status: false }]);
 
       setNewTask("");
