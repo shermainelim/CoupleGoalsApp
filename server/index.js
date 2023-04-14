5d9f2db49d1cdc73845281b901956bfaaeede49d
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-04-14 18:01:08 +0800

    EmailJS Integration for Notifying Clients upon signup
    - emails to first person and second person after registration for a couple space

--- server/index.js
+++ server/index.js
@@ -34,6 +34,29 @@ app.post("/api/sendemail", async (req, res) => {
     const reply_to = email;
     const subject = "Sign Up Message From Couple Goals Official";
     const message = `
+        <h3>Hello ${username} ,</h3>
+        <p>Thank for signing up with Couple Goals ! </p>
+        <p>Enjoy,</p>
+        <p>Couple Goals Official</p>
+    `;
+
+    await sendEmail(subject, message, send_to, sent_from, reply_to);
+    res.status(200).json({ success: true, message: "Email Sent" });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+app.post("/api/sendEmailSecond", async (req, res) => {
+  const email = req.body.secondPersonEmail;
+  const username = req.body.secondPersonName;
+
+  try {
+    const send_to = email;
+    const sent_from = process.env.EMAIL_USER;
+    const reply_to = email;
+    const subject = "Sign Up Message From Couple Goals Official";
+    const message = `
         <p>Hello ${username}</p>
         <p>Thank for signing up with Couple Goals ! </p>
         <p>Enjoy,</p>
