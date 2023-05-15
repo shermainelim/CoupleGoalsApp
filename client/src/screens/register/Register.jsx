b4d540e63851c7fd5a457611e8237520a622f45c
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-05-15 20:50:51 +0800

    Feature: slight form change

--- client/src/screens/register/Register.jsx
+++ client/src/screens/register/Register.jsx
@@ -141,7 +141,7 @@ const Register = () => {
                   })
                 );
 
-                setChangeSpaceName("");
+                
               }
             }}
           ></CustomButton>
