8a182dd54b2e5938c8bddc9be11b7b1715072ef1
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-05-16 22:38:03 +0800

    Bug: axios

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://couple-goals-new.herokuapp.com/",
+  baseURL: "https://couple-goals-new.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
